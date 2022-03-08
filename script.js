5dcd913ff705d021e8639f088ddcd5c11eae56f5
Tom Salter <tomjsalter@protonmail.com>
2022-03-08 17:14:54 +0000

    Create initial grid structure

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const container = document.querySelector('.grid-container');
 const box = document.createElement('div');
 box.classList.add('boxStyle');
 container.appendChild(box);
+
+// use createElement on each loop
