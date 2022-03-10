7c39044381b385756fadf8ff2bc6394161d87785
Tom Salter <tomjsalter@protonmail.com>
2022-03-10 18:57:51 +0000

    Create basic grid structure

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const container = document.querySelector('.grid-container');
-const box = document.createElement('div');
-box.classList.add('boxStyle');
-container.appendChild(box);
-
-// use createElement on each loop
+const gridContainer = document.getElementById("grid-container");
+gridContainer.style.display = "grid";
+gridContainer.style.height = "960px";
+gridContainer.style.width = "960px";
+gridContainer.style.gridTemplateColumns = "auto auto";
+gridContainer.style.gridTemplateRows = "auto auto";
