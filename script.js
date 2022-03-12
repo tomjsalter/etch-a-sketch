c2ebb355da3c999a6672631a481646ab8049b029
Tom Salter <tomjsalter@protonmail.com>
2022-03-12 16:26:13 +0000

    Adjust size and position of grid

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const gridContainer = document.getElementById("grid-container");
 const gridItem = document.createElement("div");
 gridContainer.style.display = "grid";
-gridContainer.style.height = "900px";
-gridContainer.style.width = "900px";
+gridContainer.style.height = "700px";
+gridContainer.style.width = "700px";
+gridContainer.style.margin = "100px auto"
 gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
 gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
 gridItem.classList.add("itemStyle");
