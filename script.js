5dd776d4ff30ef621485b5578f1b0db0e1c2649a
Tom Salter <tomjsalter@protonmail.com>
2022-03-12 15:23:07 +0000

    Apply initial style to grid item

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const gridContainer = document.getElementById("grid-container");
+const gridItem = document.createElement("div");
 gridContainer.style.display = "grid";
 gridContainer.style.height = "900px";
 gridContainer.style.width = "900px";
 gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
 gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
+gridItem.classList.add("itemStyle");
+gridContainer.appendChild(gridItem);
