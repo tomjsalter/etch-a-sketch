a54649f065ffbe25e668562ff76e6fd33b5918bf
Tom Salter <tomjsalter@protonmail.com>
2022-03-13 17:36:15 +0000

    Move grid styling from JS into CSS

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 const gridContainer = document.getElementById("grid-container");
 let gridNum = "16";
-gridContainer.style.border = "none";
-gridContainer.style.display = "grid";
-gridContainer.style.height = "700px";
-gridContainer.style.width = "700px";
-gridContainer.style.margin = "100px auto"
 gridContainer.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
 gridContainer.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
-gridContainer.appendChild(document.createElement("div"));
-
 
 function populateGrid() {
     let totalNum = parseInt(gridNum);
