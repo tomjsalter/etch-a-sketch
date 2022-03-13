3df848fa18fe7110dd92339c335dacbc1649bca3
Tom Salter <tomjsalter@protonmail.com>
2022-03-13 17:01:24 +0000

    Generate div grid-items to populate grid
    This update will use a function to auto-populate the grid with a basic
    div. Styling needs improving but functionality is working.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const gridContainer = document.getElementById("grid-container");
-let num = "80";
+let gridNum = "16";
 gridContainer.style.border = "none";
 gridContainer.style.display = "grid";
 gridContainer.style.height = "700px";
 gridContainer.style.width = "700px";
 gridContainer.style.margin = "100px auto"
-gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+gridContainer.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
 gridContainer.appendChild(document.createElement("div"));
 
 
 function populateGrid() {
-    
+    let totalNum = parseInt(gridNum);
+    totalNum = totalNum * totalNum;
+    for (let i = 0; i < totalNum; i++) {
+        gridContainer.appendChild(document.createElement("div"));
+    }
 }
+populateGrid();
