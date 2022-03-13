67662a94650ba9559156ddf57b2862f9884c33d1
Tom Salter <tomjsalter@protonmail.com>
2022-03-13 12:50:17 +0000

    Rework div creation Appended createElement on each round of loop

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const gridContainer = document.getElementById("grid-container");
-const gridItem = document.createElement("div");
+gridContainer.style.border = "none";
 gridContainer.style.display = "grid";
 gridContainer.style.height = "700px";
 gridContainer.style.width = "700px";
 gridContainer.style.margin = "100px auto"
 gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
 gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
-gridItem.classList.add("itemStyle");
-gridContainer.appendChild(gridItem);
+gridContainer.appendChild(document.createElement("div"));
+
+
+function populateGrid() {
+
+}
