d86f7761079e502fb086b477d8433e73c3c599d2
Tom Salter <tomjsalter@protonmail.com>
2022-03-13 16:35:32 +0000

    Convert grid-template to template-literal

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const gridContainer = document.getElementById("grid-container");
+let num = "80";
 gridContainer.style.border = "none";
 gridContainer.style.display = "grid";
 gridContainer.style.height = "700px";
 gridContainer.style.width = "700px";
 gridContainer.style.margin = "100px auto"
-gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
-gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
+gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 gridContainer.appendChild(document.createElement("div"));
 
 
 function populateGrid() {
-
+    
 }
