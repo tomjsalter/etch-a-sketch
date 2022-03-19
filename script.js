437adf8582221ffb057920167d48ec1b6dd218a1
Tom Salter <tomjsalter@protonmail.com>
2022-03-19 19:15:01 +0000

    Fix grid calculation error

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const gridContainer = document.getElementById("grid-container");
+const startBtn = document.querySelector(".start");
+const resetBtn = document.querySelector(".reset");
+const blackBtn = document.querySelector(".black");
+const whiteBtn = document.querySelector(".white");
+const randomBtn = document.querySelector(".random");
+const percentBtn = document.querySelector(".percent");
 let gridNum;
-gridContainer.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
-gridContainer.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
 
 function populateGrid() {
   let totalNum = parseInt(gridNum);
   totalNum = totalNum * totalNum;
+  gridContainer.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
   for (let i = 0; i < totalNum; i++) {
     const gridItem = document.createElement("div");
     gridItem.addEventListener("mouseenter", () => {
@@ -14,3 +20,8 @@ function populateGrid() {
     gridContainer.appendChild(gridItem);
   }
 }
+
+startBtn.addEventListener('click', () => {
+  gridNum = prompt("Choose a number. Maximum 100");
+  populateGrid();
+});
