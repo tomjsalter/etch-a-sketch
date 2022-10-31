44f4c85df83bec4938defd3db088114df66106a2
Tom Salter <tomjsalter@protonmail.com>
2022-10-31 15:26:35 +0000

    Apply border to grid items and container

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const gridContainer = document.querySelector("#grid-container");
+gridContainer.style.backgroundColor = "black";
+gridContainer.style.gap = "1px";
+gridContainer.style.border = "solid 1px black";
 gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
 gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
+
+for (let i = 0; i < 256; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.style.backgroundColor = "white";
+    gridContainer.appendChild(gridSquare);
+}
