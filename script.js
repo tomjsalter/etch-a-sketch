8bd9cdc80804f7cf5bf930c5dc7b83e7afcb2a34
Tom Salter <tomjsalter@protonmail.com>
2022-11-01 15:24:24 +0000

    Move reset logic into function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const generateBtn = document.querySelector("#generate-grid");
-const resetBtn = document.querySelector("#reset");
+const randomBtn = document.querySelector("#random-btn");
+const shadingBtn = document.querySelector("#shading-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const resetBtn = document.querySelector("#reset-btn");
 const gridContainer = document.querySelector("#grid-container");
 gridContainer.style.backgroundColor = "black";
 gridContainer.style.gap = "1px";
 gridContainer.style.border = "solid 1px black";
 
 function startGrid() {
+  resetGrid();
   gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
   gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
   for (let i = 0; i < 256; i++) {
@@ -16,22 +20,9 @@ function startGrid() {
   populateGrid();
 }
 
-function populateGrid() {
-  let gridItems = gridContainer.querySelectorAll("div");
-  gridItems = Array.from(gridItems);
-  gridItems.forEach((item) => {
-    item.addEventListener("mouseover", function () {
-      item.classList.remove("white-bg");
-      item.classList.add("black-bg");
-    });
-  });
-}
-
 function generateGrid() {
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.firstChild);
-  }
-  let gridNum = prompt("Choose a number", "Maximum of 100");
+  resetGrid();
+  let gridNum = prompt("Choose a number");
   let loopCount = gridNum * gridNum;
   if (gridNum > 100) {
     alert("Number choice must be below 100");
@@ -47,8 +38,33 @@ function generateGrid() {
   }
 }
 
+function populateGrid() {
+  let gridItems = gridContainer.querySelectorAll("div");
+  gridItems = Array.from(gridItems);
+  gridItems.forEach((item) => {
+    item.addEventListener("mouseover", function () {
+      // if...else in populateGrid() function
+      // check what #id value is associated with button clicked
+      // apply classList depending on button (random color, shading)
+      item.classList.remove("white-bg");
+      item.classList.add("black-bg");
+    });
+  });
+}
+
+
+function resetGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
+
 generateBtn.addEventListener("click", function () {
   generateGrid();
+});
+
+resetBtn.addEventListener("click", function() {
+  startGrid();
 });
 
 startGrid();
